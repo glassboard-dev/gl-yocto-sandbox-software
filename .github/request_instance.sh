5764ff5453e91714fca9e636e2ab92ede438ddaa
stephendpmurphy <stephenmurphy@glassboard.com>
2021-12-16 16:45:31 -0500

    Correct path for EC2 config and add jq to apt-get

--- .github/request_instance.sh
+++ .github/request_instance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start a spot request to AWS
-aws ec2 request-spot-instances --spot-price "0.75" --instance-count 1 --type "one-time" --launch-specification file://aws_ec2_config.json > spot_request_resp.json
+aws ec2 request-spot-instances --spot-price "0.75" --instance-count 1 --type "one-time" --launch-specification file://.github/aws_ec2_config.json > spot_request_resp.json
 SPOT_REQ_RESP=$(echo $?)
 SPOT_REQ_ID=$(cat spot_request_resp.json | jq '.SpotInstanceRequests[0].SpotInstanceRequestId' --raw-output)
 
